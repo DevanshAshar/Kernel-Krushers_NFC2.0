087b68a00a3705d03799b4bdded472a463363958
Dhruvin3103 <dcchawda2003@gmail.com>
2023-09-15 16:01:53 +0530

    added news model

--- backend/news/admin.py
+++ backend/news/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import news_img, news_text
 # Register your models here.
+
+admin.site.register(news_text)
+admin.site.register(news_img)
--- backend/news/models.py
+++ backend/news/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class news_img(models.Model):
+    news = models.TextField(null=True,blank=True)
+    img = models.ImageField(upload_to='news')
+    
+    
+class news_text(models.Model):
+    news = models.TextField()
+    
+    def __str__(self) -> str:
+        return self.news
