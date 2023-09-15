d9d34d4f09bda78225089200dd205e2c913c5738
Dhruvin3103 <dcchawda2003@gmail.com>
2023-09-15 12:03:09 +0530

    just added a comment

--- backend/translate/transgoogle.py
+++ backend/translate/transgoogle.py
@@ -4,7 +4,7 @@ from googletrans import Translator
 translator = Translator(user_agent="Mozilla/5.0")
 
 # Text to translate
-text_to_translate = "આજના ચોંકાવનારા સમાચાર કોલેજના એક વિદ્યાર્થીને નાસાની મુલાકાત લેવાની તક મળી"
+text_to_translate = "प्रभादेवीतील त्या राड्याची धास्ती, गणपती विसर्जनाला स्वागत मंडप नको, राजकीय पक्षांना पोलिसांची मनाई"
 
 # Translate text to English
 translated = translator.translate(text_to_translate,dest='en')
@@ -14,3 +14,5 @@ translated_text = translated.text
 
 print(f"Original text: {text_to_translate}")
 print(f"Translated text: {translated_text}")
+
+# https://cloud.google.com/translate/docs/languages
