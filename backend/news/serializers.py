5e4674788eb690ce692c68d5e40b5a114c4611d6
Dhruvin3103 <dcchawda2003@gmail.com>
2023-09-15 23:35:48 +0530

    changed the file structure

--- backend/news/serializers.py
+++ backend/news/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from news.ocr_example import image_to_text
 from news.translate import google_translate
 from .models import news_text, news_img
-
+from ML.pickle.categorize import predict_category
 class newsTextSerializer(serializers.ModelSerializer):
     class Meta:
         model = news_text
@@ -21,7 +21,7 @@ class newsImgSerializer(serializers.ModelSerializer):
     #overiding the create method to store text extracted by the image
     def create(self, validated_data):
         data = self.context.get('request').data
-        print(data['lang'], 'data 12345')
+        # print(data['lang'], 'data 12345')
         instance = super().create(validated_data)
         text = image_to_text("http://127.0.0.1:8000/media/"+str(instance.img),data['lang'])
         instance.news = google_translate(text)
@@ -29,5 +29,6 @@ class newsImgSerializer(serializers.ModelSerializer):
         return instance
     
     def to_representation(self, instance):
-        print(instance)
+        print(instance.news,'instance')
+        
         return super().to_representation(instance)
