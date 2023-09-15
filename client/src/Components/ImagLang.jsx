0ed0edf67feee283b608351de81947c86f787d5e
Dhruvin3103 <dcchawda2003@gmail.com>
2023-09-15 20:42:15 +0530

    added a component for image to lang

---
+++ client/src/Components/ImagLang.jsx
@@ -0,0 +1,64 @@
+const languagesData = [
+    {
+        _id: 0,
+        name: "English",
+        value: "eng"
+    },
+    {
+        _id: 1,
+        name: "Hindi",
+        value: "hin"
+    },
+    {
+        _id: 2,
+        name: "Urdu",
+        value: "urd"
+    },
+    {
+        _id: 3,
+        name: "Punjabi",
+        value: "pan"
+    },
+    {
+        _id: 4,
+        name: "Gujarati",
+        value: "guj"
+    },
+    {
+        _id: 5,
+        name: "Marathi",
+        value: "mar"
+    },
+    {
+        _id: 6,
+        name: "Telugu",
+        value: "tel"
+    },
+    {
+        _id: 7,
+        name: "Kannada",
+        value: "kan"
+    },
+    {
+        _id: 8,
+        name: "Malayalam",
+        value: "mal"
+    },
+    {
+        _id: 9,
+        name: "Tamil",
+        value: "tam"
+    },
+    {
+        _id: 10,
+        name: "Bengali",
+        value: "ben"
+    },
+    {
+        _id: 11,
+        name: "Assamese",
+        value: "asm"
+    }
+];
+
+export const Languages = languagesData;
