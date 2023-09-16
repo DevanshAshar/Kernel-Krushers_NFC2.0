04ab8b5657387fb194924db3fc3dbfef886bd1ba
Devansh Ashar <ashar.devansh@gmail.com>
2023-09-16 05:54:40 +0530

    hoatLayout editted

--- client/src/Components/HostLayout.jsx
+++ client/src/Components/HostLayout.jsx
@@ -1,8 +1,52 @@
-import React from "react"
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import toast from "react-hot-toast";
 
-export default function HostLayout()
-{
-    return (
-        <h1>Host Layout Page goes here.</h1>
-    );
-}
+const HostLayout = () => {
+  const [textData, setTextData] = useState([]);
+  const [imgData, setImgData] = useState([]);
+  const getData = async () => {
+    try {
+      const res = await axios.get(`${process.env.REACT_APP_API}/news/text/`);
+      setTextData(res.data);
+      const res1 = await axios.get(`${process.env.REACT_APP_API}/news/img/`);
+      setImgData(res1.data);
+    } catch (error) {
+      console.log(error.message);
+      toast.error("Something went wrong");
+    }
+  };
+  useEffect(() => {
+    getData();
+  }, []);
+  return (
+    <>
+      {textData && (
+        <div>
+          <h2>Textual News</h2>
+          <div className="card">
+            {textData.map((textItem) => (
+              <div className="card">
+                <div className="card-body">{textItem.news}</div>
+                <p>{textItem.category}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
+      {imgData && (
+        <div>
+          <h2>Image news</h2>
+          {imgData.map((imgItem) => (
+            <div className="card">
+              <img src={imgItem.img} alt={null} className="card-img-top" />
+              <p>{imgItem.category}</p>
+            </div>
+          ))}
+        </div>
+      )}
+    </>
+  );
+};
+
+export default HostLayout;
--- client/src/Pages/Login.jsx
+++ client/src/Pages/Login.jsx
@@ -1,7 +1,11 @@
 import axios from "axios"
 import React from "react"
+import { useSearchParams } from "react-router-dom"
 
 export default function Login() {
+    let [searchParams, setSearchParams] = useSearchParams();
+    let warning = searchParams.get("message");
+
     const [loginFormData, setLoginFormData] = React.useState({ email: "", password: "" })
 
     async function handleSubmit(e) {
@@ -21,6 +25,7 @@ export default function Login() {
     return (
         <div className="login-container">
             <h1>Sign in to your account</h1>
+            {warning ? <h4 className="warning">{warning}</h4> : null}
             <form onSubmit={handleSubmit} className="login-form">
                 <input
                     name="email"
